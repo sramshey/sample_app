a278284709f459fbb4639f2b64d5136fc6f23e94
Stefan Amshey <sramshey@gmail.com>
2012-05-10 12:01:09 -0700

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,19 +3,27 @@ require 'spec_helper'
 describe "StaticPages" do
   
   describe "Home page" do
+    
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
     
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+    
   end
   
   describe "Help Page" do
+    
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_content('Help')
@@ -25,9 +33,11 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
     end
+    
   end
 
   describe "About page" do
+    
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title', :text => "About Us")
@@ -38,6 +48,7 @@ describe "StaticPages" do
       page.should have_selector('title',
         :text => 'Ruby on Rails Tutorial Sample App | About Us')
     end
+    
   end
   
 end
